65addfc6fa6d3e4c93710aeee790336c6836c319
mitsubishievo99 <89307932+mitsubishievo99@users.noreply.github.com>
2021-08-22 21:13:39 +0200

    updated datatypes

--- collatz_cpp/collatzz.cpp
+++ collatz_cpp/collatzz.cpp
@@ -1,32 +1,32 @@
 #include "pch.h"
 #include "framework.h"
-#include "collatzz.h"
+#include "collatz.h"
 #include <math.h>
 
-COLLATZZ_API char get_first_digit(unsigned long long val) {
+unsigned char get_first_digit(unsigned long long val) {
 
-	char digit, first_digit;
-	long power = 1;
+	unsigned char digit, first_digit;
+	unsigned long long power = 1;
 
 	// already fast
-	digit = (char)log10(val);
+	digit = (unsigned char)log10(val);
 
-	// faster than math.pow
-	for (char i = 0; i < digit; ++i)
+	// faster than math.log10
+	for (unsigned char i = 0; i < digit; ++i)
 		power *= 10;
 
-	first_digit = (char)(val / power);
+	first_digit = (unsigned char)(val / power);
 
 	return first_digit;
 }
 
-COLLATZZ_API void get_digit_count(unsigned long long* buf, unsigned long long val) {
+void get_digit_count(unsigned long long* buf, unsigned long long val) {
 
 	unsigned long long value;
 	unsigned long long digit_count[9] = {};
 
 	// loop over every number from 1 to N
-	for (long i = 1; i < val + 1; i++) {
+	for (unsigned long long i = 1; i < val + 1; i++) {
 		value = i;
 
 		digit_count[get_first_digit(value) - 1] += 1;
