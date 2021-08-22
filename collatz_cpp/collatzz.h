7b10f99e049256bf148547f5efdcd68f57c5b332
mitsubishievo99 <89307932+mitsubishievo99@users.noreply.github.com>
2021-08-22 21:14:07 +0200

    updated datatypes

--- collatz_cpp/collatzz.h
+++ collatz_cpp/collatzz.h
@@ -4,5 +4,5 @@
 #define COLLATZZ_API __declspec(dllimport)
 #endif
 
-extern "C" COLLATZZ_API char get_first_digit(unsigned long long val);
-extern "C" COLLATZZ_API void get_digit_count(unsigned long long* buf, unsigned long long n);
+extern "C" COLLATZ_API unsigned char get_first_digit(unsigned long long val);
+extern "C" COLLATZ_API void get_digit_count(unsigned long long* buf, unsigned long long n);
