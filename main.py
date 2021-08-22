1a2e4fc685bed6ed37c235ee2903e3da932e5f0f
mitsubishievo99 <89307932+mitsubishievo99@users.noreply.github.com>
2021-08-23 01:19:15 +0200

    stop active calculation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-VERSION = '0.1.2'
+VERSION = '0.1.5'
 
 import sys
 import ctypes
+
+from numpy.core.arrayprint import dtype_is_implied
 import icon_rc
 import numpy as np
 import pyqtgraph as pg
@@ -22,40 +24,42 @@ pg.setConfigOption('background', '#f0f0f0')
 pg.setConfigOption('foreground', 'k')
 
 # import custom c function
-collatz = ctypes.CDLL("lib/collatz.dll")
+collatz = ctypes.CDLL('lib/collatz.dll')
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.spinBox.setValue(1337)
-        self.horizontalSlider.setMaximum(10000)
 
         # set title
         self.setWindowTitle('Collatz Conjecture Visualized V'+VERSION)
 
         # connect signals
         self.actionAbout.triggered.connect(self.about)
+        self.actionStopCalculation.triggered.connect(self.terminate_thread)
         self.spinBox.valueChanged.connect(self.plot_single_sequence)
 
         # configure first plotWidget 
         self.plotWidget.showGrid(x=True, y=True, alpha=0.4)
-        self.plotWidget.setLabels(title='A single Sequence', left='Value N', bottom='Iteration')
+        self.plotWidget.setLabels(title='single Sequence', left='Value N', bottom='iteration')
         self.curveSingleSequence = self.plotWidget.plot(symbol='o', symbolSize=4, pen=pg.mkPen('r', width=1))
 
         # configure second plotWidget
         self.plotHisto.showGrid(x=False, y=True, alpha=0.4)
-        self.plotHisto.setLabels(title='Digit count in full Sequence', left='Count', bottom='Digit')
+        self.plotHisto.setLabels(title='digit count of full Sequence', left='count', bottom='digit')
         self.BarItem = pg.BarGraphItem(x=np.arange(1,10), height=0, width=0.5, brush=(0,0,255,110))
         self.plotHisto.addItem(self.BarItem)
 
         # configure third plotWidget
         self.plotLog.showGrid(x=True, y=True, alpha=0.4)
         self.plotLog.setLogMode(False, True)
-        self.plotLog.setLabels(title='Log of single Sequence', left='Log(N)', bottom='Iteration')
+        self.plotLog.setLabels(title='log of single Sequence', left='log10(N)', bottom='iteration')
         self.curveLogarithmicSequence = self.plotLog.plot(pen=pg.mkPen('#0072FF', width=1))
         self.curveLogarithmicTrend = pg.InfiniteLine(angle=0, pos=0, pen=pg.mkPen('r', width=1))
         self.plotLog.addItem(self.curveLogarithmicTrend)
+
+        self.targetItem2 = pg.TargetItem(size=4, symbol='o', label='max={1:0,.0f}', labelOpts={'color':'#0061FF', 'offset':(4,2)})
+        self.plotWidget.addItem(self.targetItem2)
 
         # create thread for heavy load (compute digit count in full sequence)
         self.thread = QtCore.QThread()
@@ -74,7 +78,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.spinBox.valueChanged.emit(self.spinBox.value())
 		
     def status_update(self, status):
-        self.plotHisto.setLabels(title='Digit count in full Sequence'+' '+status)
+        self.plotHisto.setLabels(title='digit count of full sequence'+' '+status)
 
     def plot_single_sequence(self, val):
         """
@@ -88,19 +92,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             Starting Number of Sequence
         """
         new_value = val
-        y_values = np.array([new_value])
+        y_values = np.array([new_value], dtype=ctypes.c_ulonglong)
 
         while new_value != 1:
             if new_value%2 == 0:
-                new_value = new_value/2
+                new_value = new_value//2
             else:
                 new_value = 3*new_value+1
-            y_values = np.append(y_values, int(new_value))
+            y_values = np.append(y_values, new_value)
 
         x_values = np.arange(1, len(y_values)+1)
 
         self.curveSingleSequence.setData(x_values, y_values)
         self.curveLogarithmicSequence.setData(x_values, y_values)
+        self.targetItem2.setPos((np.where(y_values == np.max(y_values))[0]+1, np.max(y_values)))
 
         if self.checkGraph.isChecked():
             self.plotWidget.setRange(xRange=x_values, yRange=[1, max(y_values)])
@@ -141,16 +146,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def about(self):
         QMessageBox.about(self, "Um was geht es?", '<html><head/><body><p><span style=" font-size:9pt; color:#000000;">&quot;Das Collatz-Problem, auch als (3n+1)-Vermutung bezeichnet, ist ein ungelöstes mathematisches Problem, das 1937 von Lothar Collatz gestellt wurde&quot;<br/></span></p><p><span style=" font-size:9pt; color:#000000;">Bei dem Problem geht es um Zahlenfolge, die nach einem einfachen Bildungsgesetz konstruiert werden:<br/>→ Beginne mit irgendeiner natürlichen Zahl</span><span style=" font-size:9pt; font-weight:600; color:#000000;"> n &gt; 0<br/></span><span style=" font-size:9pt; color:#000000;">→ Ist </span><span style=" font-size:9pt; font-weight:600; color:#000000;">n</span><span style=" font-size:9pt; color:#000000;"> gerade, so nimm als nächstes </span><span style=" font-size:9pt; font-weight:600; color:#000000;">n/2<br/></span><span style=" font-size:9pt; color:#000000;">→ Ist </span><span style=" font-size:9pt; font-weight:600; color:#000000;">n</span><span style=" font-size:9pt; color:#000000;"> ungerade, so nimm als nächstes </span><span style=" font-size:9pt; font-weight:600; color:#000000;">3n+1<br/></span><span style=" font-size:9pt; color:#000000;">→ Wiederhole die Vorgehensweise mit der erhaltenen Zahl<br/></span></p><p><span style=" font-size:9pt; color:#000000;">Quellen:<br/></span><a href="https://de.wikipedia.org/wiki/Collatz-Problem"><span style=" font-size:9pt; text-decoration: underline; color:#1E00FF;">Wikipedia Eintrag<br/></span></a><a href="https://www.youtube.com/watch?v=094y1Z2wpJg&amp;amp;ab_channel=Veritasium"><span style=" font-size:9pt; text-decoration: underline; color:#1E00FF;">Veritasium auf YouTube</span></a></p></body></html>')
+    
+    def terminate_thread(self):
+        if self.worker.busy is True:
+            self.thread.terminate()
+            self.thread.wait()
+            self.status_update('(terminated)')
+            self.plot_digit_count(np.linspace(0, 0, 9, dtype=int))
+            self.thread.start()
 
     # reimplement closeEvent method
     def closeEvent(self, event):
-        self.thread.quit()
+        self.thread.terminate()
         self.thread.wait()
 
 class CalculateValues(QObject):
 
     data   = pyqtSignal(list)
     status = pyqtSignal(str)
+    busy = False
 
     @pyqtSlot(int)
     def compute_digit_count(self, val):
@@ -163,6 +177,7 @@ class CalculateValues(QObject):
         val : int
             Starting Number of Sequence
         """
+        self.busy = True
         self.status.emit('(calculating...)')
 
         # C unsigned long long array which is our buffer
@@ -171,8 +186,9 @@ class CalculateValues(QObject):
         # C Function implementation: void get_digit_count(unsigned long long *buf, unsigned long long val){...}
         collatz.get_digit_count(digit_count, val)
 
+        self.busy = False
         self.status.emit('')
-        self.data.emit(digit_count[:])
+        self.data.emit(digit_count[:]) # Bug: every second item is zero?
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -180,4 +196,3 @@ if __name__ == "__main__":
     win = MainWindow()
     win.show()
     sys.exit(app.exec())
-    
