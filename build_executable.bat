b21abb36cb23fc127edc9573646ccef84bec4fed
mitsubishievo99 <florian.weissensteiner@gmail.com>
2021-08-26 19:12:04 +0200

    updated path

--- build_executable.bat
+++ build_executable.bat
@@ -1,4 +1,4 @@
 CALL ..\Scripts\activate.bat
-CALL pyinstaller -w -F -n "Collatz" --distpath executable --icon="resources\3n1.ico" --add-data="resources\3n1.ico;." --add-binary="lib\collatz.dll;." main.py
+CALL pyinstaller -w -F -n "Collatz" --distpath executable --icon="resources\3n1.ico" --add-data="resources\3n1.ico;." --add-binary="collatz_c\x64\Release\collatz.dll;." main.py
 
 cmd \k
